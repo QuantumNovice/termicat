df6b16c3624923e637b667846c5143482314006c
QuantumNovice <portabl3lapy@gmail.com>
2020-05-01 09:07:54 +0500

    Added doctests

--- termicat.py
+++ termicat.py
@@ -1,5 +1,5 @@
-import numpy as np
-#from PIL import Image
+# import numpy as np
+# from PIL import Image
 from colr import Colr as C
 import cv2
 import os
@@ -13,31 +13,35 @@ import time
 #     return data
 
 
-#image = load_image('test.jpg')
+# image = load_image('test.jpg')
 
 
 def termicat(img):
     img = cv2.imread(img)
-    w,h = os.get_terminal_size()
-    sf = min(w,h)
+    w, h = os.get_terminal_size()
+    sf = min(w, h)
     res = cv2.resize(img, dsize=(sf, sf), interpolation=cv2.INTER_CUBIC)
 
-
     for i, pixel_line in enumerate(res):
-        str = ''
-        for j, (r,g,b) in enumerate(pixel_line):
-            str+=C().b_rgb(r,g,b).rgb(0, 0, 0, ' ')
+        str = ""
+        for j, (r, g, b) in enumerate(pixel_line):
+            str += C().b_rgb(r, g, b).rgb(0, 0, 0, " ")
         print(str)
+
 
 def main():
     t1 = time.time()
-    if len(sys.argv)!=0:
+    if len(sys.argv) != 0:
         for img in sys.argv[1:]:
             termicat(img)
     else:
-        print('termicat [img1 img2 ...]')
+        print("termicat [img1 img2 ...]")
     t2 = time.time()
-    print(f'Time:{t2-t1}s')
+    print(f"Time:{t2-t1}s")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
     main()
